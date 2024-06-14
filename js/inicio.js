61fd953092d63f782b4d63953576231dee3663f8
Tania <oert070904mmclyna2@soycecytem.mx>
2024-06-13 21:51:05 -0600

    Movimientos, estructura

--- js/inicio.js
+++ js/inicio.js
@@ -79,15 +79,57 @@ const tablaSaldo = () => {
 //FUNCIONES PARA BOTÓN MOVIMIENTOS
 
 btnMov.onclick=()=>{
-    let saldoT = `
+    let movimientosC = `
     <h3>¿QUÉ MOVIMIENTO DESEAS HACER HOY?</h3>
     <br>
-    <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addRetiro">
-        Retiro
-    </button>
-    <br>
-    <br>
+    <table class="table w-100 m-auto">
+    <tr>
+    <td><button class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#addRetiro">
+        Retiro  
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-piggy-bank-fill" viewBox="0 0 16 16">
+        <path d="M7.964 1.527c-2.977 0-5.571 1.704-6.32 4.125h-.55A1 1 0 0 0 .11 6.824l.254 1.46a1.5 1.5 0 0 0 1.478 1.243h.263c.3.513.688.978 1.145 1.382l-.729 2.477a.5.5 0 0 0 .48.641h2a.5.5 0 0 0 .471-.332l.482-1.351c.635.173 1.31.267 2.011.267.707 0 1.388-.095 2.028-.272l.543 1.372a.5.5 0 0 0 .465.316h2a.5.5 0 0 0 .478-.645l-.761-2.506C13.81 9.895 14.5 8.559 14.5 7.069q0-.218-.02-.431c.261-.11.508-.266.705-.444.315.306.815.306.815-.417 0 .223-.5.223-.461-.026a1 1 0 0 0 .09-.255.7.7 0 0 0-.202-.645.58.58 0 0 0-.707-.098.74.74 0 0 0-.375.562c-.024.243.082.48.32.654a2 2 0 0 1-.259.153c-.534-2.664-3.284-4.595-6.442-4.595m7.173 3.876a.6.6 0 0 1-.098.21l-.044-.025c-.146-.09-.157-.175-.152-.223a.24.24 0 0 1 .117-.173c.049-.027.08-.021.113.012a.2.2 0 0 1 .064.199m-8.999-.65a.5.5 0 1 1-.276-.96A7.6 7.6 0 0 1 7.964 3.5c.763 0 1.497.11 2.18.315a.5.5 0 1 1-.287.958A6.6 6.6 0 0 0 7.964 4.5c-.64 0-1.255.09-1.826.254ZM5 6.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0"/>
+      </svg>
+    </button></td>
+    <td><button class="btn btn-success mx-2" data-bs-toggle="modal" data-bs-target="#addTransferencia">
+        Transferencia 
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-down" viewBox="0 0 16 16">
+        <path d="M12.5 9a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7m.354 5.854 1.5-1.5a.5.5 0 0 0-.708-.708l-.646.647V10.5a.5.5 0 0 0-1 0v2.793l-.646-.647a.5.5 0 0 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+        <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+      </svg>
+    </button></td>
+   <td> <button class="btn btn-info mx-2" onclick="historialT()">
+       Historial de Transacciones
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
+  <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7 7 0 0 0-.985-.299l.219-.976q.576.129 1.126.342zm1.37.71a7 7 0 0 0-.439-.27l.493-.87a8 8 0 0 1 .979.654l-.615.789a7 7 0 0 0-.418-.302zm1.834 1.79a7 7 0 0 0-.653-.796l.724-.69q.406.429.747.91zm.744 1.352a7 7 0 0 0-.214-.468l.893-.45a8 8 0 0 1 .45 1.088l-.95.313a7 7 0 0 0-.179-.483m.53 2.507a7 7 0 0 0-.1-1.025l.985-.17q.1.58.116 1.17zm-.131 1.538q.05-.254.081-.51l.993.123a8 8 0 0 1-.23 1.155l-.964-.267q.069-.247.12-.501m-.952 2.379q.276-.436.486-.908l.914.405q-.24.54-.555 1.038zm-.964 1.205q.183-.183.35-.378l.758.653a8 8 0 0 1-.401.432z"/>
+  <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z"/>
+  <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5"/>
+</svg>
+    </button></td>
+    </tr>
+    </table>
+  
 `;
-action.innerHTML = saldoT;
+action.innerHTML = movimientosC;
 
+}
+
+
+const historialT=()=>{
+    let tablaH = `<table class="table table-striped w-75 m-auto">
+    <tr>
+    <td>TIPO DE TRANSACCIÓN</td>
+    <td>DESCRIPCIÓN</td>
+    <td>MONTO</td>
+    <td>FECHA Y HORA</td>
+    </tr>`;
+        let saldoGuardado = JSON.parse(localStorage.getItem("saldo")) || [];
+        saldoGuardado.forEach(item => {
+            tablaSaldo += `
+    <tr>
+    <td>$${item.monto.toFixed(2)}</td>
+    <td>${item.fechaHora}</td>
+    </tr>`;
+        });
+        tablaSaldo += `</table>`;
+        action.innerHTML += tablaH;
 }
